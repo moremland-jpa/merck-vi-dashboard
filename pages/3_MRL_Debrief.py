57cb166816beec75a4fac2d86e75e996eaef802d
moremland-jpa <moremland@jpa.com>
2026-09-04 09:37:17 -0400

    sidebar fix

--- pages/3_MRL_Debrief.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.session_state["selected_workstream"] = "MRL Debrief"
-st.switch_page("pages/1_Workstreams.py")
--- pages/4_Asset_Reporting.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.session_state["selected_workstream"] = "Asset Reporting"
-st.switch_page("pages/1_Workstreams.py")
